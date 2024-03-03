c935795709ac943f4e36ffc8b5ca05391a4b4c71
Vaibhav Wani <vaibhavwani1509@gmail.com>
2024-03-03 23:10:00 +0530

    Added Expandable Card

--- app/src/main/java/com/vaibhavwani/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/vaibhavwani/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,5 +1,9 @@
 package com.vaibhavwani.mealzapp.ui.meals
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,11 +11,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
@@ -35,8 +51,12 @@ fun MealsCategoriesScreen() {
 fun CategoryCard(
     category: Category
 ) {
+    val isExpanded = remember { mutableStateOf(false) }
+    val maxLines by animateIntAsState(if (isExpanded.value) 10 else 4, label = "")
     Card(
-        modifier = Modifier.padding(
+        modifier = Modifier
+            .animateContentSize()
+            .padding(
             vertical = 8.dp,
             horizontal = 16.dp,
         ),
@@ -44,7 +64,10 @@ fun CategoryCard(
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(
+                    vertical = 16.dp
+                ),
             verticalAlignment = Alignment.CenterVertically
         ) {
             AsyncImage(
@@ -52,7 +75,27 @@ fun CategoryCard(
                 model = category.image,
                 contentDescription = null
             )
-            Text(text = category.name ?: "")
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = category.name ?: "",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Text(
+                    text = category.description ?: "",
+                    maxLines = maxLines,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
+            Icon(
+                modifier = Modifier
+                    .align(Alignment.Top)
+                    .clickable { isExpanded.value = !isExpanded.value }
+                    .padding(16.dp),
+                imageVector = if (isExpanded.value) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                contentDescription = null
+            )
         }
     }
 }
