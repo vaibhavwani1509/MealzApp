327c6d02dff288b701bbd454fc743995e243f732
Vaibhav Wani <vaibhavwani1509@gmail.com>
2024-03-04 12:55:24 +0530

    Minor Rafactor

--- app/src/main/java/com/vaibhavwani/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/vaibhavwani/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,21 +1,13 @@
 package com.vaibhavwani.mealzapp.ui.meals
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
@@ -24,17 +16,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
@@ -107,7 +94,6 @@ fun ExpandableCard(
                         if (isExpanded) {
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
-                            top.linkTo(parent.top)
                             bottom.linkTo(titleRef.top)
                             width = Dimension.fillToConstraints
                             height = Dimension.wrapContent
@@ -115,7 +101,6 @@ fun ExpandableCard(
                         } else {
                             start.linkTo(parent.start)
                             end.linkTo(titleRef.start)
-                            top.linkTo(parent.top)
                             bottom.linkTo(parent.bottom)
                             width = Dimension.wrapContent
                             height = Dimension.wrapContent
